08fb45b991702d30db6dda9bc117707e18cf3fa5
dhanushboga <dhanushboga555@gmail.com>
2026-04-20 21:51:35 +0530

    message

---
+++ expense/route53.tf
@@ -0,0 +1,13 @@
+resource "aws_route53_zone" "example" {
+  name = "daws84s.com"
+}
+
+
+resource "aws_route53_record" "expense" {
+  count = length(var.instance_names)
+  zone_id = aws_route53_zone.example.zone_id
+  name    = "${var.instance_names[count.index]}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.expense[count.index].private_ip]
+}
